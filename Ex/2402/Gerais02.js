59dcccb5d8bf41d8bfbb906eaa601320d2010863
UtescherIntrieri <18ianaui1999@gmail.com>
2023-02-24 16:02:55 -0300

    .

---
+++ Ex/2402/Gerais02.js
@@ -0,0 +1,39 @@
+// 1.5 Funções
+// 1. Escreva uma função que receba um número e retorne seu dobro.
+// 2. Escreva uma função que receba um número e retorne seu quadrado.
+// 3. Escreva uma função que receba nome, idade, endereço e retorne uma
+// mensagem no formato: "Olá, eu sou X, tenho X ano(s) e moro em X". Faça
+// uso da interpolação de strings.
+// 4. Escreva uma função que receba dia, mês e ano e retorne a data por
+// extenso no formato "DD/MM/AAAA".
+
+// 5. Escreva uma função que receba um preço de um produto e um percen-
+// tual. Retorne o valor aplicado ao desconto.
+
+// 6. Escreva uma função que receba uma palavra e um número. Retorne a
+
+// palavra repetida a quantidade de vezes indicada pelo segundo parâme-
+// tro. Exemplo: repetir("batata", 3) -> "batatabatatabatata". OBS: Utilize
+
+// um loop para resolver.
+// 7. Escreva uma função que recebe outra função e invoca ela.
+// 8. Transforme os itens 1, 2 e 3 em arrow function expressions.
+// 1.6 Strings
+
+// 1. Escreva uma função que receba uma string e retorne o número de ca-
+// racteres contidos nela.
+
+// 2. Escreva uma função que receba uma string e retorne a mesma string em
+// letras maiúsculas.
+// 3. Escreva uma função que receba uma string e retorne a mesma string em
+// letras minúsculas.
+// 4. Escreva uma função que receba duas strings e retorne true se elas forem
+// iguais ou false caso contrário.
+// 5. Escreva uma função que receba uma string e um número e retorne os
+// últimos caracteres da string de acordo com o número informado.
+// 6. Escreva uma função que recebe uma frase e uma palavra. Retorne a frase
+// com as ocorrências da palavra censuradas/trocadas por ***. Exemplo:
+// censura("Eu jogo lol e valorant, mas lol é melhor", "lol") -> "Eu jogo ***
+// e valorant, mas *** é melhor". Dica: use a função replaceAll.
+// 7. Escreva uma função que receba uma string e um caractere e retorne o
+// número de ocorrências desse caractere na string.
---
+++ Ex/2402/oi.js
@@ -0,0 +1,52 @@
+// Exercício I: Crie uma função que recebe 2 números e um operador (+, -, *, /)
+// e faça a operação indicada entre os 2 números. Não tem retorno.
+function conta(n1,n2,op) {
+  var res
+  switch (op) {
+    case "+" :
+      return n1+n2
+    break;
+    case "-" :
+      return n1+n2
+    break;
+    case "*" : 
+      return n1+n2
+    break;
+    case "/" :
+      return n1+n2
+    break;
+    default:
+      console.log("operador inválido");
+  }
+}
+console.log(conta(5,4,"+"));
+console.log("\n");
+
+// Exercício II: Crie uma função que converte fahrenheit em celsius
+// C = (F - 32) / 1.8. Retorna a temperatura convertida.
+// Em seguida crie uma outra função (checarTemperatura) que recebe uma temperatura
+// como parâmetro:
+// Caso esteja abaixo de 11: "Muito frio!"
+// Esteja entre 11 e 15: "Clima bom!"
+// Esteja entre 16 e 30: "Agradável..."
+// Seja maior que 30: "Hell"
+
+function converter(f) {
+  var c = (f-32)/1.8
+  console.log(`${f}F = ${c.toFixed(2)} C°`);
+ 
+  checarT(c)
+  
+  function checarT(c) {
+    if (c<11) {
+      console.log("Muito Frio");
+    } else if(c>=11 && c<16){
+      console.log("Clima Bom");
+    } else if(c>=16 && c<31){
+      console.log("Agradável...");
+    } else if(c>31){
+      console.log("Hell");
+    }
+  }
+} 
+converter(0)
