ec27e21a8c7583a67e218aeea474277e42f0151c
UtescherIntrieri <18ianaui1999@gmail.com>
2023-03-23 15:05:47 -0300

    .

--- React/Práticas3/src/components/CompFilhoB/CompFilhoB.jsx
+++ React/Práticas3/src/components/CompFilhoB/CompFilhoB.jsx
@@ -1,9 +1,23 @@
+import {Form} from 'react-bootstrap'
+import { useContext } from "react";
 import "./CompFilhoB.css";
+import { BatataContext } from "../../contexts/BatataContext";
+
 
 export function CompFilhoB() {
+  // Capturando os dados do "value" do Provider
+  const resultado = useContext(BatataContext);
+
   return (
     <div className="filho-b">
       <h3>FILHO B</h3>
+      <Form className="m-2">
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder={resultado} />
+        </Form.Group>
+      </Form>
+      <br />
     </div>
   );
 }
--- React/Práticas3/src/components/CompFilhoD/CompFilhoD.jsx
+++ React/Práticas3/src/components/CompFilhoD/CompFilhoD.jsx
@@ -1,9 +1,19 @@
+import { useContext } from "react";
+import { BatataContext } from "../../contexts/BatataContext";
+import {Badge} from 'react-bootstrap/';
 import "./CompFilhoD.css";
 
 export function CompFilhoD() {
+  // Capturando os dados do "value" do Provider
+  const resultado = useContext(BatataContext);
+
   return (
     <div className="filho-d">
       <h3>FILHO D</h3>
+      <p>
+        <b>Valor: </b>
+        <Badge bg="secondary">{resultado}</Badge>
+      </p>
     </div>
   );
 }
