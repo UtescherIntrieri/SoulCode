2832de521472945c348a14ff1be7605f4601ba25
UtescherIntrieri <18ianaui1999@gmail.com>
2023-03-31 09:31:04 -0300

    .

--- React/Práticas4/bibliotech/src/firebase/config.js
+++ React/Práticas4/bibliotech/src/firebase/config.js
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 // Chaves de acesso ao firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCGV-hKd1DwX0wKcmlJV65SF1V2m1Bo9eY",
-  authDomain: "bibliotech-aulas-241dd.firebaseapp.com",
-  projectId: "bibliotech-aulas-241dd",
-  storageBucket: "bibliotech-aulas-241dd.appspot.com",
-  messagingSenderId: "455282911729",
-  appId: "1:455282911729:web:37e3cca50849ce62b73c50",
+  apiKey: "AIzaSyAQAkemHIEzJ1JK-70dCaG4onHH10SmvRI",
+  authDomain: "bibliotech-nai.firebaseapp.com",
+  projectId: "bibliotech-nai",
+  storageBucket: "bibliotech-nai.appspot.com",
+  messagingSenderId: "650664067450",
+  appId: "1:650664067450:web:4b57b17ebb1ac4396956c3"
 };
 
 // Inicializa o app com base nas configurações acima
